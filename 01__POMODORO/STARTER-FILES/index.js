6d3bc09997de2fe2d592afce31c2835adc3ffe55
Adwait <adwait.kul.2018@gmail.com>
2022-12-09 17:22:18 -0800

    Removed commented code

--- 01__POMODORO/STARTER-FILES/index.js
+++ 01__POMODORO/STARTER-FILES/index.js
@@ -8,10 +8,6 @@ startButton.addEventListener("click", countdown);
 function countdown() {
   startButton.innerHTML = "stop";
   countDownClock();
-
-  //   startButton.innerHTML = "stop";
-  //   startButton.removeEventListener("click", changeToStop);
-  //   startButton.addEventListener("click", changeToStart);
 }
 
 function countDownClock() {
@@ -36,37 +32,3 @@ function countDownMinutes() {
   setTimeout(countDownMinutes, 60000);
   document.querySelector("#minutesInput").value = minutes;
 }
-
-// function breakCountdown() {
-//   if (seconds < 0 && minutes >= 0) {
-//     minutes--;
-//     seconds = 59;
-//   }
-//   seconds--;
-//   if (seconds >= 0 && minutes >= 0) {
-//     setTimeout(breakCountdown, 1000);
-//   } else {
-//     stopCountdown();
-//   }
-// }
-
-// function stopCountdown() {
-//   clearTimeout(setTimeout(countDown, 1000));
-// }
-
-// function changeToStart() {
-//   startButton.innerHTML = "start";
-//   startButton.removeEventListener("click", changeToStart);
-//   startButton.addEventListener("click", changeToStop);
-// }
-
-// function countdownMinutes() {
-//   minutes--;
-//   document.querySelector("#minutesInput").value = minutes;
-// }
-
-// function countdownSeconds() {
-//   seconds = 59;
-//   seconds--;
-//   document.querySelector("#secondsInput").value = seconds;
-// }
