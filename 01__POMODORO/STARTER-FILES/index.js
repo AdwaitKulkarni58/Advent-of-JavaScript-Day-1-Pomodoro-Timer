5306e78cabf8d54427cfd09d91ae6adb947b6db6
Adwait <adwait.kul.2018@gmail.com>
2022-12-09 16:56:01 -0800

    Added seconds countdown

--- 01__POMODORO/STARTER-FILES/index.js
+++ 01__POMODORO/STARTER-FILES/index.js
@@ -2,22 +2,64 @@ let startButton = document.querySelector(".start");
 let minutes = document.querySelector("#minutesInput").value;
 let seconds = document.querySelector("#secondsInput").value;
 
-startButton.addEventListener("click", changeToStop);
+startButton.addEventListener("click", countdown);
 
-function changeToStop() {
-  setInterval(countdownMinutes, 60000);
-  startButton.innerHTML = "stop";
-  startButton.removeEventListener("click", changeToStop);
-  startButton.addEventListener("click", changeToStart);
+function countdown() {
+  countDownSeconds();
+  if (seconds < 0 && minutes >= 0) {
+    minutes--;
+    seconds = 59;
+  }
+  //   if (seconds > 0 && minutes >= 0) {
+  //     setTimeout(countdown, 1000);
+  //   } else {
+  //     setTimeout(breakCountdown, 1000);
+  //   }
+  //   startButton.innerHTML = "stop";
+  //   startButton.removeEventListener("click", changeToStop);
+  //   startButton.addEventListener("click", changeToStart);
 }
 
-function changeToStart() {
-  startButton.innerHTML = "start";
-  startButton.removeEventListener("click", changeToStart);
-  startButton.addEventListener("click", changeToStop);
+function countDownSeconds() {
+  if (seconds <= 0) {
+    seconds = 60;
+    document.querySelector("#secondsInput").value = seconds;
+  }
+  seconds--;
+  setTimeout(countDownSeconds, 1000);
+  document.querySelector("#secondsInput").value = seconds;
 }
 
-function countdownMinutes() {
-  minutes--;
-  document.querySelector("#minutesInput").value = minutes;
-}
+// function breakCountdown() {
+//   if (seconds < 0 && minutes >= 0) {
+//     minutes--;
+//     seconds = 59;
+//   }
+//   seconds--;
+//   if (seconds >= 0 && minutes >= 0) {
+//     setTimeout(breakCountdown, 1000);
+//   } else {
+//     stopCountdown();
+//   }
+// }
+
+// function stopCountdown() {
+//   clearTimeout(setTimeout(countDown, 1000));
+// }
+
+// function changeToStart() {
+//   startButton.innerHTML = "start";
+//   startButton.removeEventListener("click", changeToStart);
+//   startButton.addEventListener("click", changeToStop);
+// }
+
+// function countdownMinutes() {
+//   minutes--;
+//   document.querySelector("#minutesInput").value = minutes;
+// }
+
+// function countdownSeconds() {
+//   seconds = 59;
+//   seconds--;
+//   document.querySelector("#secondsInput").value = seconds;
+// }
