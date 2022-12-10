ec3be9ca91f0bd090bfbfa2836522a143f93a302
Adwait <adwait.kul.2018@gmail.com>
2022-12-09 18:51:28 -0800

    Added comments to explain functions

--- 01__POMODORO/STARTER-FILES/index.js
+++ 01__POMODORO/STARTER-FILES/index.js
@@ -1,29 +1,35 @@
+// Declaring variables
 let timer;
 let startButton;
 let circleRing;
 let userMinutes;
 let userSeconds;
 
+// Enter the minutes and seconds you want, default is 15 minutes
 function getUserInput() {
   timer = document.querySelector(".timer");
   startButton = document.querySelector(".start");
   circleRing = document.querySelector(".ring");
+
   userMinutes = sessionStorage.getItem("userMinutes");
   userMinutes = prompt("Enter minutes.");
   sessionStorage.setItem("userMinutes", userMinutes);
   userSeconds = sessionStorage.getItem("userSeconds");
   userSeconds = prompt("Enter seconds.");
   sessionStorage.setItem("userSeconds", userSeconds);
+
   document.querySelector("#minutesInput").value = userMinutes;
   document.querySelector("#secondsInput").value = userSeconds;
   startButton.addEventListener("click", countdown);
 }
 
+// Since its pomodoro, you cannot stop the timer once started
 function countdown() {
   timer.removeChild(startButton);
   countDownClock();
 }
 
+// The actual countdown function
 function countDownClock() {
   if (userMinutes == 0 && userSeconds == 0) {
     document.querySelector("#secondsInput").value = 00;
@@ -42,6 +48,7 @@ function countDownClock() {
   document.querySelector("#secondsInput").value = userSeconds;
 }
 
+// Function to decrement the minutes
 function countDownMinutes() {
   setTimeout(countDownMinutes, 60000);
   document.querySelector("#minutesInput").value = userMinutes;
