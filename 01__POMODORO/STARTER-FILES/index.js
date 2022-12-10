48109fb47f22e69e6093b6fdf2802c0b17bc6c28
Adwait <adwait.kul.2018@gmail.com>
2022-12-09 16:10:31 -0800

    Added minutes countdown ability

--- 01__POMODORO/STARTER-FILES/index.js
+++ 01__POMODORO/STARTER-FILES/index.js
@@ -1,8 +1,11 @@
 let startButton = document.querySelector(".start");
+let minutes = document.querySelector("#minutesInput").value;
+let seconds = document.querySelector("#secondsInput").value;
 
 startButton.addEventListener("click", changeToStop);
 
 function changeToStop() {
+  setInterval(countdownMinutes, 60000);
   startButton.innerHTML = "stop";
   startButton.removeEventListener("click", changeToStop);
   startButton.addEventListener("click", changeToStart);
@@ -12,4 +15,9 @@ function changeToStart() {
   startButton.innerHTML = "start";
   startButton.removeEventListener("click", changeToStart);
   startButton.addEventListener("click", changeToStop);
+}
+
+function countdownMinutes() {
+  minutes--;
+  document.querySelector("#minutesInput").value = minutes;
 }
